3051134e5d92694ee2f223a7c5d6485379f2858f
Rahim <abderrahim.hadjoudj@utoulouse.fr>
2026-03-12 16:36:12 +0100

    Complete game logic

--- src/Game.cpp
+++ src/Game.cpp
@@ -2,22 +2,27 @@
 #include <ctime>
 #include <cstdlib>
 #include <ncurses.h>
+
+
+
 Game::Game(size_t width, size_t height)
 {
     _number_of_mines = 0;
     _number_of_flags = 0;
+    _has_lost = false;
+	_has_won = false;
     _width = width;
     _height = height;
-    _Grid.reserve(_width);
-    for (size_t i = 0; i < _width; i++)
-    {
-        _Grid[i].reserve(_height);
-        _Grid.push_back(std::vector<Cell>());
-        for (size_t j = 0; j < _height; j++)
-        {
-            _Grid[i].push_back(Cell());
-        }
-    }
+	_Grid.reserve(_width);
+		for (size_t i = 0; i < _width; i++)
+		{
+    		_Grid.push_back(std::vector<Cell>());
+   			_Grid[i].reserve(_height);
+   				 for (size_t j = 0; j < _height; j++)
+    	{
+      	  _Grid[i].push_back(Cell());
+   		}
+}
 }
 Game::~Game()
 {
@@ -55,16 +60,22 @@ void Game::begin(size_t mines)
 void Game::discover(size_t x, size_t y)
 {
     size_t discovered = 0;
-    if (x >= 0 && x < _Grid.size() && y >= 0 && y < _Grid[x].size())
+
+    if (x < _Grid.size() && y < _Grid[x].size())
     {
         _Grid[x][y].discover(_Grid, x, y);
+
+        if (_Grid[x][y].is_a_mine() && _Grid[x][y].is_discovered())
+            _has_lost = true;
+
         for (size_t i = 0; i < _Grid.size(); i++)
         {
-            for (size_t j = 0; j < _Grid[x].size(); j++)
+            for (size_t j = 0; j < _Grid[i].size(); j++)
             {
                 if (_Grid[i][j].is_discovered())
                 {
                     discovered++;
+
                     if (_Grid[i][j].is_flagged())
                     {
                         _number_of_flags++;
@@ -73,6 +84,9 @@ void Game::discover(size_t x, size_t y)
                 }
             }
         }
+
+        if (discovered == (_width * _height - _number_of_mines) && !_has_lost)
+            _has_won = true;
     }
 }
 void Game::draw()
@@ -107,19 +121,19 @@ void Game::draw()
 }
 int Game::get_width()
 {
-    return 1;
+    return _width;
 }
 int Game::get_height()
 {
-    return 1;
+    return _height;
 }
 bool Game::has_lost()
 {
-    return false;
+    return _has_lost;
 }
 bool Game::has_won()
 {
-    return false;
+    return _has_won;
 }
 void Game::add_flag(size_t x, size_t y)
 {
